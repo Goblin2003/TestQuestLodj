57dd281f5011992b0cf0565d242c1ae9f3965ead
MoreNip <Filip-lax2016@yandex.ru>
2025-11-01 21:55:43 +0300

    Добавьте файлы проекта.

---
+++ searshFreeIP/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace searshFreeIP
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            var ips = new List<string> { "1.2.3.5", "1.2.3.4", "1.2.3.9" };
+            Console.WriteLine(FindSmallestFreeIp(ips)); 
+        }
+        static string FindSmallestFreeIp(List<string> usedIps)
+        {
+            // Преобразуем IP в числа для быстрого сравнения
+            var used = usedIps
+                .Select(ip => BitConverter.ToUInt32(IPAddress.Parse(ip).GetAddressBytes().Reverse().ToArray(), 0))
+                .OrderBy(x => x)
+                .ToList();
+
+            for (int i = 0; i < used.Count - 1; i++)
+            {
+                uint nextIp = used[i] + 1;
+                // Пропускаем адреса, оканчивающиеся на 0
+                if ((nextIp & 0xFF) == 0)
+                    nextIp++;
+
+                if (nextIp < used[i + 1])
+                {
+                    var bytes = BitConverter.GetBytes(nextIp).Reverse().ToArray();
+                    return new IPAddress(bytes).ToString();
+                }
+            }
+
+            // Если свободного не нашли — возвращаем следующий за последним
+            uint lastNext = used.Last() + 1;
+            if ((lastNext & 0xFF) == 0)
+                lastNext++;
+            var lastBytes = BitConverter.GetBytes(lastNext).Reverse().ToArray();
+            return new IPAddress(lastBytes).ToString();
+        }
+
+        
+
+    }
+}
---
+++ tast2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.FileIO;
+using System.Net;
+
+
+namespace tast2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            
+            
+            using (TextFieldParser csvv = new TextFieldParser(@"C:\Users\filip\source\repos\searshFreeIP\tast2\csv\geo-US.csv"))
+            {
+                csvv.TextFieldType = FieldType.Delimited;
+                csvv.SetDelimiters(",");
+                while (!csvv.EndOfData)
+                {
+                    var f = csvv.ReadFields();
+                    var prefix = f[0].Split('/')[1];
+                    Console.WriteLine($"\"{prefix}\" \"{f[3]}\", \"{f[4]}\"");
+
+                }
+
+            }
+            
+        }
+    }
+}
