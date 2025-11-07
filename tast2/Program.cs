34ffaf53922ed3e3f0aed4ad7d298e79bbfd3e17
MoreNip <Filip-lax2016@yandex.ru>
2025-11-07 16:13:00 +0300

    Исправления

--- tast2/Program.cs
+++ tast2/Program.cs
@@ -7,30 +7,47 @@ using System.Globalization;
 using Microsoft.VisualBasic;
 using Microsoft.VisualBasic.FileIO;
 using System.Net;
+using MySql.Data.MySqlClient;
 
 
 namespace tast2
 {
     internal class Program
     {
+        
         static void Main(string[] args)
             
         {
-            
-            
-            using (TextFieldParser csvv = new TextFieldParser(@"C:\Users\filip\source\repos\searshFreeIP\tast2\csv\geo-US.csv"))
-            {
-                csvv.TextFieldType = FieldType.Delimited;
-                csvv.SetDelimiters(",");
-                while (!csvv.EndOfData)
-                {
-                    var f = csvv.ReadFields();
-                    var prefix = f[0].Split('/')[1];
-                    Console.WriteLine($"\"{prefix}\" \"{f[3]}\", \"{f[4]}\"");
+            string connectionString = "server=127.0.0.1;port=3306;username=root;password=;database=tesquest";
+            string query = "SELECT IP, sity, state FROM geo_us";
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabse =new MySqlCommand(query, databaseConnection);
+            commandDatabse.CommandTimeout = 60;
+            MySqlDataReader reader;
 
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabse.ExecuteReader();
+                while (reader.Read()) 
+                { 
+                    string ip = reader.GetString("ip");
+                    string sity = reader.GetString("sity");
+                    string state = reader.GetString("state");
+                    string[] parts = ip.Split('/');
+                    string mask = (parts.Length > 1) ? parts[1] : "";
+                    Console.WriteLine($"{mask}\t{sity}\t{state}");
                 }
 
+
+                databaseConnection.Close();
             }
+            catch (Exception ex) 
+            { 
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+            
             
         }
     }
